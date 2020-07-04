eea5f769e653590788f8f750be30019335f95dc9
Zageron <hello@adambryant.ca>
2020-07-03 20:56:44 -0700

    More wip

--- examples/redirect-flow/src/index.ts
+++ examples/redirect-flow/src/index.ts
@@ -2,22 +2,22 @@ import express = require("express");
 import * as dotenv from "dotenv";
 import { format as formatUrl } from "url";
 import { AddressInfo } from "net";
+import { OAuthRedirectParameters } from "../../../dist/patreon"
 
 dotenv.config();
 
-const HOST: string = "localhost:8080";
-const PORT: number = 8080;
+const HOST: string = "localhost:8081";
+const PORT: number = 8081;
 const REDIR: string = "/oauth/redirect";
 
 const redirectUri = formatUrl({
     protocol: "http",
     host: HOST,
-    pathname: REDIR
+    pathname: REDIR,
 });
 
 const clientId = process.env.PATREON_CLIENT_ID;
 //const clientSecret = process.env.PATREON_CLIENT_SECRET;
-
 
 //import requestPromise = require("request-promise-native");
 
@@ -34,22 +34,33 @@ const loginUrl: string = formatUrl({
 });
 
 const app: express.Express = express();
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, () =>
+{
     const port: AddressInfo = server.address() as AddressInfo;
     console.log(`Listening on http://localhost:${port.port}`);
 });
 
-app.get("/", (_req, res) => {
+app.get("/", (_req, res) =>
+{
     res.send(`<a href="${loginUrl}">Link with Patreon</a>`);
 });
 
-app.get('/oauth/redirect', (req, res) => {
-    const { code } = req.query
+
+app.get("/oauth/redirect", (req, res) =>
+{
+    const
+        {
+            code,
+            state
+        }: OAuthRedirectParameters = req.query;
     //let token: string;
+    
+    const error: string = "invalid";
+    console.log(`${code || error}, ${state || error}`);
+    res.send(`${code || error}, ${state || error}`);
 
-    console.log(`${code}`);
-    res.send(`Denied`);
-
+    //POST www.patreon.com/api/oauth2/token
+    // Request access token with grants and such.
 
     // return oauthClient.getTokens(code, redirect)
     //     .then(({ access_token }) => {
--- src/patreon.ts
+++ src/patreon.ts
@@ -1,1 +1,70 @@
-export async function doSomething() {}
+export function doSomething(): string
+{
+    return "";
+}
+
+interface BaseParameters
+{
+    client_id: string;
+    client_secret: string;
+    grant_type: string;
+}
+
+export interface OAuthRedirectParameters
+{
+    code?: string;
+    state?: string;
+}
+
+// interface GetTokenParameters extends BaseParameters
+// {
+//     code: string;
+//     redirect_uri: string;
+// }
+
+// interface UpdateTokenParameters extends BaseParameters
+// {
+//     refresh_token: string;
+//     grant_type: string;
+// }
+
+interface ErrorDefinitions
+{
+    INVALID_GRANT: string;
+    INVALID_CLIENT: string;
+}
+
+const ERRORS: ErrorDefinitions = {
+    INVALID_GRANT: "invalid_grant",
+    INVALID_CLIENT: "invalid_client",
+};
+
+function ProcessErrors(err: string, params: BaseParameters): string
+{
+    let errorString: string;
+
+    switch (err)
+    {
+        case ERRORS.INVALID_GRANT:
+            errorString = `Invalid grant_type: ${params.grant_type}`;
+            break;
+        case ERRORS.INVALID_CLIENT:
+            errorString = `Invalid client_id: ${params.client_id}`;
+            break;
+
+        default:
+            errorString = `Unknown error: ${err}`;
+            break;
+    }
+
+    return errorString;
+}
+
+const emptyParams: BaseParameters =
+{
+    client_id: "",
+    client_secret: "",
+    grant_type: ""
+};
+
+ProcessErrors("", emptyParams);
